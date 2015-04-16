1a534ea3689099aad4d1fed5d1bd9630560df0cd
Xerato <admin@xerato.com>
2015-04-16 10:08:57 +0900

    대략적인 레이아웃구성

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server)
+ ;;;
+
+server.listen(80);
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.on('send message', function (data) {
+        io.sockets.emit('new message', data);
+    });
+})
